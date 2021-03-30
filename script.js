7c3c7e007e2abc3ad107df745be666dc0a82469b
Marco Peters <m.peters646@gmail.com>
2021-03-30 11:00:14 +0200

    Added excersise files

---
+++ script.js
@@ -0,0 +1,60 @@
+const sinterklaasIsGul = true;
+
+// Promise maken (dit hoeven jullie dus niet te leren, dit is aan de API (back-end) kant)
+const krijgIkEenNieuweiPhone = new Promise((resolve, reject) => {
+  if (sinterklaasIsGul) {
+    const smartphone = {
+      merk: 'Apple',
+      type: 'iPhone 11',
+    };
+    resolve(smartphone);
+  } else {
+    const metWelkeReden = new Error('Je bent stout geweest, geen cadeaus voor jou');
+    reject(metWelkeReden);
+  }
+});
+
+// Promise aanroepen, of "consumeren" (dit zullen je dus wel doen en moeten leren)
+const vraagAanSinterklaas = () => {
+  krijgIkEenNieuweiPhone
+    .then(function (resolved) {
+      // yay, je hebt een nieuwe smartphone
+      console.log(resolved);
+    })
+    .catch(function (error) {
+      // oeps, geen Sinterklaas cadeau dit jaar
+      console.log(error.message);
+    });
+};
+
+// vraagAanSinterklaas();
+
+/* --------------------------------------
+           Excersise 1 
+-----------------------------------------*/
+
+const testNum = (num) => {
+  return new Promise((resolve, reject) => {
+    if (num > 10) {
+      resolve(`${num} is greater then 10`);
+    } else {
+      reject(`${num} is less then 10`);
+    }
+  });
+};
+
+testNum(18)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((message) => {
+    console.log(message);
+  });
+
+testNum(8)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((message) => {
+    console.log(message);
+  });
